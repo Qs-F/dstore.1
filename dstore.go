50aa4aa836b783c3e7d9fcbb28feddd217efdeaa
Qs-F <qsf@de-liker.com>
2018-12-23 11:32:06 +0900

    Fix chan struct{} to chan bool

--- dstore.go
+++ dstore.go
@@ -20,10 +20,10 @@ type Searcher interface {
 }
 
 type Storer interface {
-	Get(ctx context.Context, key Key, lock chan struct{}) (value chan Value, err error)
-	Put(ctx context.Context, key Key, value Value, lock chan struct{}) (err error)
-	Remove(ctx context.Context, key Key, lock chan struct{}) (err error)
-	Search(ctx context.Context, search Searcher, lock chan struct{}) (err error)
+	Get(ctx context.Context, key Key, lock chan bool) (value chan<- Value, err error)
+	Put(ctx context.Context, key Key, value Value, lock chan bool) (err error)
+	Remove(ctx context.Context, key Key, lock chan bool) (err error)
+	Search(ctx context.Context, search Searcher, lock chan bool) (err error)
 	Open(ctx context.Context) (Store Storer, err error)
 	Ping(ctx context.Context) (err error)
 	Close(ctx context.Context) (err error)
